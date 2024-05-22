42b819ef06ae7be0a64e32af0f945f99647562f1
Türkalp Kucur <turkalp.kucur@gmail.com>
2024-05-22 16:19:49 +0300

    first commit
    first commit

---
+++ nArchitectureDemo/src/corePackages/core.Persistence/Dynamic/Sort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace core.Persistence.Dynamic;
+
+public class Sort
+{
+  public string Field { get; set; }
+  public string Direction { get; set; }
+  public Sort()
+  {
+    Field = string.Empty;
+    Direction = string.Empty;
+  }
+  public Sort(string field, string @operator)
+  {
+    Field = string.Empty;
+    Direction = string.Empty;
+  }
+
+
+
+}
---
+++ nArchitectureDemo/src/corePackages/core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,46 @@
+﻿using System.Linq.Expressions;
+using core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace core.Persistence.Repositories;
+public interface IRepository<TEntity, TEntityId> : IQueryable<TEntity> where TEntity : Entity<TEntityId> 
+{
+  Task<TEntity> Get(
+
+      Expression<Func<TEntity, bool>> predicate,
+      Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+      bool withDeleted = false,
+      bool enableTracking = true);
+
+
+  Task<Paginate<TEntity>> GetList(
+
+      Expression<Func<TEntity, bool>> predicate = null,
+      Func<IQueryable<TEntity>, IOrderedQueryable<TEntity>>? orderBy = null,
+      Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+      int index = 0,
+      int size = 10,
+      bool withDeleted = false,
+      bool enableTracking = true);
+
+  Task<Paginate<TEntity>> GetListByDynamic(
+    DynamicQuery dynamic,
+    Expression<Func<TEntity, bool>>? predicate = null,
+    Func<IQueryable<TEntity>, IIncludableQueryable<TEntity, object>>? include = null,
+    int index = 0,
+    int size = 10,
+    bool withDeleted = false,
+    bool enableTracking = true);
+
+  Task<bool> Any(
+    Expression<Func<TEntity, bool>>? predicate = null,
+    bool withDeleted = false,
+    bool enableTracking = true);
+
+  Task<TEntity> Add(TEntity entity);
+  Task<ICollection<TEntity>> AddRange(ICollection<TEntity> entity);
+  Task<TEntity> Update(TEntity entity);
+  Task<ICollection<TEntity>> UpdateRange(ICollection<TEntity> entity);
+  Task<TEntity> Delete(TEntity entity, bool permanent = false);
+  Task<ICollection<TEntity>> DeleteRange(ICollection<TEntity> entity, bool permanent = false);
+}
